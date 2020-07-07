c31ac8b0b1a7f369ff0d39f6a0b50bf3e7db76ae
WeenaTF <67097499+WeenaTF@users.noreply.github.com>
2020-07-07 06:28:01 +0300

    Update makeCacheMatrix_cacheSolve.R

--- makeCacheMatrix_cacheSolve.R
+++ makeCacheMatrix_cacheSolve.R
@@ -1,12 +1,18 @@
 makeCacheMatrix<- function(x= matrix()) {
+    #Set the cached inverse varible "inv" to NULL
   inv<- NULL
+    # This function initilizes the "Special" Matrix
   set<- function(y){
     x<<-y
     inv<<-NULL
   }
+    # This function return the matrix itself
   get <- function()x
+   # This function store the calculated inverse
   setInverse <- function(inverse) inv<<- inverse
+    # This function retriev the cached inverse
   getInverse <- function () inv
+    # Return this "special" matrix
   list(set=set,
        get = get,
        setInverse=setInverse,
@@ -14,13 +20,16 @@ makeCacheMatrix<- function(x= matrix()) {
 }
 
 cacheSolve <- function (x, ...){
+    # Get the stored value from the special matrix.
   inv <- x$getInverse()
+    # The value is not NULL indicating that the value have already calculated. 
   if(!is.null(inv)){
     message("getting cached data")
     return (inv)
   }
   mat<- x$get()
   inv<- solve(mat, ...)
+  
   x$setInverse(inv)
 }
 
